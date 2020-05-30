42bcc477c87d4891c19eec8a8d4d79265cd89edf
Sandra Filipsson <sandra.filipsson@connectitude.se>
2020-05-30 20:04:21 +0200

    some structure

--- src/content/WelcomePage.js
+++ src/content/WelcomePage.js
@@ -13,41 +13,45 @@ function WelcomePage() {
         console.log("Click for correction of contacts");
     };
 
-    return (
-        <>
-            <Hero>
-                <img src={logo} className="app-logo" alt="logo" />
-            </Hero>
-            <Container>
-                <Section direction="column">
+    const renderHeader = () => {
+        return (
+            <Section direction="column">
+                <Headline
+                    size="xl"
+                    title="Hej Bert-Åke"
+                    text="Hej Bert-Åke" />
+            </Section>);
+    };
+
+    const renderContactNumber = () => {
+        return (
+            <Section direction="column">
+                <Section direction="row" style={{ marginBottom: "0px" }}>
                     <Headline
-                        size="xl"
-                        title="Hej Bert-Åke"
-                        text="Hej Bert-Åke" />
+                        size="sm"
+                        weight="bold"
+                        title="Mobilnummer"
+                        text="Mobilnummer" />
+                    <Headline
+                        style={{ marginLeft: ".4rem" }}
+                        size="sm"
+                        title="0700 00 00 00"
+                        text="0700 00 00 00" />
                 </Section>
 
-                <Section direction="column">
-                    <Section direction="row" style={{ marginBottom: "0px" }}>
-                        <Headline
-                            size="sm"
-                            weight="bold"
-                            title="Mobilnummer"
-                            text="Mobilnummer" />
-                        <Headline
-                            style={{ marginLeft: ".4rem" }}
-                            size="sm"
-                            title="0700 00 00 00"
-                            text="0700 00 00 00" />
-                    </Section>
+                <Button
+                    style={{ marginTop: "-.2rem" }}
+                    onClick={onClickIncorrectContacts}
+                    type="text"
+                    text="Mina uppgifter stämmer inte"
+                    title="Mina uppgifter stämmer inte" />
+            </Section>
+        );
+    };
 
-                    <Button
-                        style={{ marginTop: "-.2rem" }}
-                        onClick={onClickIncorrectContacts}
-                        type="text"
-                        text="Mina uppgifter stämmer inte"
-                        title="Mina uppgifter stämmer inte" />
-                </Section>
-
+    const renderInformationalText = () => {
+        return (
+            <>
                 <Section direction="column" style={{ marginTop: "1rem" }}>
                     <Headline
                         size="m"
@@ -61,11 +65,28 @@ function WelcomePage() {
                         För att vi ska kunna ge dig en så bra upplevelse som möjligt så vill vi att du ska fylla i när du inte vill bli störd, bekräfta uppgifterna vi har om dig och godkänna avtalet.
                     </p>
                 </Section>
+            </>);
+    };
 
+    const renderGDPRConsent = () => {
+        return (
+            <Checkbox id="gdpr" checked={confirmedGdpr} onCheckedChange={setConfirmedGdpr}>
+                <p>Genom att klicka in rutan godkänner du att vi sparar din data enligt GDPR och att du läst och förstått <a href="#">villkoren</a>.</p>
+            </Checkbox>
+        );
+    };
+
+    return (
+        <>
+            <Hero>
+                <img src={logo} className="app-logo" alt="logo" />
+            </Hero>
+            <Container>
+                {renderHeader()}
+                {renderContactNumber()}
+                {renderInformationalText()}
                 <Scheduler selection={selection} onSelectionChanged={setSelection} />
-                <Checkbox id="gdpr" checked={confirmedGdpr} onCheckedChange={setConfirmedGdpr}>
-                    <p>Genom att klicka in rutan godkänner du att vi sparar din data enligt GDPR och att du läst och förstått <a href="#">villkoren</a>.</p>
-                </Checkbox>
+                {renderGDPRConsent()}
             </Container>
         </>
     );
